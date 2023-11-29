8af7fe99d1ac34dffab3a304cfc0c6d15b6781de
hamalxd <sammanhamal06@gmail.com>
2023-11-29 14:00:13 +0545

    Added Navigation

---
+++ components/Navigation.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { usePathname } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+
+const Navigation = () => {
+    const [isRouting, setIsRouting] = useState(false);
+    const path = usePathname()
+    const [isActive, setIsActive] = useState(path);
+    const [prevPath, setPrevPath] = useState("/");
+
+    useEffect(() => {
+
+      if (prevPath !== path) {
+        setIsRouting(true)
+      }
+
+    }, [path , prevPath])
+
+
+    useEffect(() => {
+      if(isRouting) {
+        setPrevPath(path)
+        const timeout = setTimeout (() => {
+          setIsRouting(false)
+        }, 1200)
+
+        return() => clearTimeout(timeout)
+      }
+    }, [isRouting])
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navigation
