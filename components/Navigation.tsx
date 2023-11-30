cf6c403a6b7198df8473bc19cd98320479747695
hamalxd <sammanhamal06@gmail.com>
2023-11-30 14:14:40 +0545

    Added Navigation animation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,6 +4,7 @@ import { NavLinks } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import Transition from "./Transition";
 
 const Navigation = () => {
   const [isRouting, setIsRouting] = useState(false);
@@ -33,6 +34,7 @@ const Navigation = () => {
       style={{ left: "20%" }}
       className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full flex justify-between items-center border border-white py-7 bg-black"
     >
+      {isRouting && <Transition />}
       {NavLinks.map((nav) => (
         <Link key={nav.name} href={nav.link} className="mb-16 pl-4 min-w-[20%]">
           <nav.icon
