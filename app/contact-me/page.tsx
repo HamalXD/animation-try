1c478b352391d7a23deabe74fc9c5050d814852a
hamalxd <sammanhamal06@gmail.com>
2023-12-06 22:48:28 +0545

    Added Contact-Forms Page

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -1,9 +1,18 @@
+import ContactsForm from '@/components/ContactsForm'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      Contact
+    <div 
+    style={{backgroundImage: "url(/bg-3.jpg)"}}
+    className='w-screen h-screen bg-cover bg-center flex items-center justify-center'>
+      <div
+      style={{backgroundImage: "url(autobg-com.webp)"}} 
+      className='h-[60%] w-[80%] relative bg-cover rounded-xl border border-white'>
+        <div className='absolute left-20 bottom-16 md:w-[30%]'>
+          <ContactsForm />
+        </div>
+      </div>
     </div>
   )
 }
